327236c6baa68a4e32cf42a6e58700395d6f2fc7
bisper1 <bisper@naver.com>
2022-03-11 18:05:51 +0900

    usecsv 모듈 생성.

---
+++ usecsv.py
@@ -0,0 +1,27 @@
+import csv
+import re
+
+
+def open_csv(filename):
+    f = open(filename, 'r')
+    reader = csv.reader(f)
+    output = []
+    for i in reader:
+        output.append(i)
+    return output
+
+
+def write_csv(filename, the_list):
+    with open(filename, 'w', newline='') as f:
+        a = csv.writer(f, delimiter=',')
+        a.writerows(the_list)
+
+
+def switch(listname):
+    for i in listname:
+        for j in i:
+            try:
+                i[i.index(j)] = float(re.sub(',', '', j))
+            except:
+                pass
+    return listname
